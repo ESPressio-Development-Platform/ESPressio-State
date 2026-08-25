74f0c6c9f067ea6299636c9fa4367b98f005de93
Simon J Stuart <lakraven@gmail.com>
2026-08-25 21:49:33 +0200

    feat(#3,#4): expose subscriptions and transport contracts

--- src/ESPressio_State.hpp
+++ src/ESPressio_State.hpp
@@ -3,3 +3,5 @@
 #include "ESPressio_DeviceIdentifier.hpp"
 #include "ESPressio_StateContract.hpp"
 #include "ESPressio_RemoteStateManager.hpp"
+#include "ESPressio_StateSubscription.hpp"
+#include "ESPressio_StateTransport.hpp"
