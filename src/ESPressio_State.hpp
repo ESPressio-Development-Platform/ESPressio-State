ee158b8f545e898f9ef86e0eeb734fa93efe04a5
Simon J Stuart <lakraven@gmail.com>
2026-08-25 23:39:51 +0200

    feat(#6): expose State observer contracts

--- src/ESPressio_State.hpp
+++ src/ESPressio_State.hpp
@@ -2,6 +2,7 @@
 
 #include "ESPressio_DeviceIdentifier.hpp"
 #include "ESPressio_StateContract.hpp"
+#include "ESPressio_StateObservers.hpp"
 #include "ESPressio_RemoteStateManager.hpp"
 #include "ESPressio_StateSubscription.hpp"
 #include "ESPressio_StateTransport.hpp"
