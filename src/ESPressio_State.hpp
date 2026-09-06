53d20978db4a579e8f8eb22db467bcb31f2b0f91
Simon J Stuart <lakraven@gmail.com>
2026-09-06 18:10:55 +0200

    Publish State runtime epoch API

--- src/ESPressio_State.hpp
+++ src/ESPressio_State.hpp
@@ -2,6 +2,7 @@
 
 #include "ESPressio_DeviceIdentifier.hpp"
 #include "ESPressio_StateContract.hpp"
+#include "ESPressio_StateRuntimeEpoch.hpp"
 #include "ESPressio_StateAddress.hpp"
 #include "ESPressio_StateAvailability.hpp"
 #include "ESPressio_StateComparison.hpp"
