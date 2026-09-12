598ef088faeb813f8ce1cba57780af42a5c03642
Simon J Stuart <lakraven@gmail.com>
2026-09-12 13:32:37 +0200

    Expose State transport binding on Runtime

--- src/ESPressio_StateRuntime.hpp
+++ src/ESPressio_StateRuntime.hpp
@@ -7,6 +7,7 @@
 #include "ESPressio_StatePersistence.hpp"
 #include "ESPressio_StateRemoteReplica.hpp"
 #include "ESPressio_StateRuntimeConfiguration.hpp"
+#include "ESPressio_StateTransportBinding.hpp"
 namespace ESPressio::State {
 namespace Detail {
 template<class TState,class TFirst,class... TRest>
@@ -96,6 +97,16 @@ public:
         static_assert(!std::is_void_v<C>,"State Type is not configured in this Runtime");
         if(_initialized) return StateRuntimeStatus::Frozen;
         const auto status=StateTypeRuntime<TState>::Get().BindPersistence(binding.View());
+        if(status!=StateRuntimeStatus::Success) _configurationError=true;
+        return status;
+    }
+    template<class TState,class Format>
+    StateRuntimeStatus BindTransport(StateTransportBinding<TState,Format>& binding) noexcept {
+        using C=Detail::ConfigurationForT<TState,TConfigurations...>;
+        static_assert(!std::is_void_v<C>,"State Type is not configured in this Runtime");
+        static_assert(TState::IsTransmissibleState,"Only TransmissibleState Types may bind transport adapters");
+        if(_initialized) return StateRuntimeStatus::Frozen;
+        const auto status=StateTypeRuntime<TState>::Get().BindTransport(binding.View());
         if(status!=StateRuntimeStatus::Success) _configurationError=true;
         return status;
     }
